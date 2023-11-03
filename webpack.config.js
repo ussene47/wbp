246746f8891cc12b19c44ce33f2aafc7166fa314
ussene47 <husseindegea47@gmail.com>
2023-11-03 11:19:58 -0700

    first commit of webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path")
+
+module.exports = {
+    entry:"./src/index.js",
+    output:{
+        path:path.resolve(__dirname,"public"),
+        filename:"bundle.js"
+    },
+
+    module:{
+
+        rules:[
+
+            {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            use:"babel-loader"
+            },
+
+            {
+                test:/\.css$/,
+                use:['style-loader', 'css-loader']
+            }
+            
+        
+        
+        ],
+
+            
+    }
+     
+
+}
